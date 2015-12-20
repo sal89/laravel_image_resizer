362403a798e52a2411b8c237513cf1bd1b8e81a4
Salman <salmanamjad89@gmail.com>
2015-12-20 13:02:23 +1100

    Synced proj

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 * `git clone https://github.com/sal89/laravel_image_resizer.git projectname`
 * `cd projectname`
-* `npm install && composer install`
+* `npm install && composer install & gulp`
 * `php artisan key:generate`
 * Create a database and inform *.env*
 * `php artisan migrate` to create tables
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,6 +5,7 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link href="/css/app.css" rel="stylesheet" type="text/css">
+        <link href="/js/datatables/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="container">
